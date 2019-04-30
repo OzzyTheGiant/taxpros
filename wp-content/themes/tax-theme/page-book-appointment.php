56ab423efeb4d992fbb65779c3d70c69f94c5fe3
Oziel Perez <OZLperez11@gmail.com>
2019-04-30 16:33:01 -0500

    Created book-appointment page with styles

---
+++ wp-content/themes/tax-theme/page-book-appointment.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<main class="segment container-gray" id="book-appointment">
+	<?php if (have_posts()) :
+		while (have_posts()) : the_post(); ?>
+	<h1><?php the_title(); ?></h1>
+	<form id="book-appointment-form" class="container-dark">
+		<input type="hidden" name="honeypot" value=""/>
+		<input type="text" name="name" placeholder="Full Name"/>
+		<input type="text" name="email" placeholder="Email Address"/>
+		<input type="text" name="phone" placeholder="Phone Number"/>
+		<input type="text" name="appointment_time" placeholder="Appointment Date & Time"/>
+		<textarea placeholder="Your message"></textarea>
+		<button type="submit" class="button main">Book Appointment</button>
+	</form>
+	<?php endwhile; endif; ?>
+</main>
+<?php get_footer(); ?>
