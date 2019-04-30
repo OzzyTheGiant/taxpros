57de43cec1e9691498bf98b1afa88ef029e8f15f
Oziel Perez <OZLperez11@gmail.com>
2019-04-30 11:41:15 -0500

    Added latest articles to home page

--- wp-content/themes/tax-theme/index.php
+++ wp-content/themes/tax-theme/index.php
@@ -44,8 +44,34 @@
 				</div>
 			</div>
 		</div>
-		<div class="segment container-dark" id="article-list">
-			
+		<div class="segment container-gray" id="article-list">
+			<h2>Latest Articles</h2>
+			<div class="row">
+				<?php
+				$query = new WP_Query([
+					"numberposts" => 4,
+					"post_status" => 'publish',
+					"meta_query" => [
+						'key' => '_thumbnail_id',
+						"value" => "0",
+						"compare" => ">"
+					]
+				]);
+
+				if ($query->have_posts()) :
+					while ($query->have_posts()) : $query->the_post() ?>
+					<div class="col-tb-6 col-xs-12">
+						<a class="post-card" href="<?php the_permalink(); ?>">
+							<?php the_post_thumbnail('full'); ?>
+							<h3><?php the_title(); ?></h3>
+							<p><?php the_date(); ?></p>
+						</a>
+					</div>
+					<?php endwhile; ?>
+				<?php else : ?>
+					<p class="col-xs-12">The posts could not be loaded. Try refreshing the page.</p>
+				<?php endif; wp_reset_postdata(); ?>
+			</div>
 		</div>
 		<?php get_template_part("partials/call-to-action", "page"); ?>
 	</main>
